b2275583d11152c9d6ee7ae0a93d22c3dcbf4055
Kevin Clark <kclark@tcpmutt.com>
2021-04-18 20:54:31 -0600

    harvester

---
+++ main.js
@@ -0,0 +1,8 @@
+module.exports.loop = function () {
+    for(let name in Game.creeps) {
+        var creep = Game.creeps[name];
+        if(creep.memory.role == 'harvester') {
+            roleHarvester.run(creep);
+        }
+    }
+}
